7915ef9f46a5ffe6af9efd5995b7b05f9e16ef3f
Park jaemin <hungryshark4292@gmail.com>
2025-03-17 23:13:06 +0900

    main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import requests
@@ -9,61 +9,13 @@ from langchain_ollama import OllamaEmbeddings
 import ollama
 from news import AITimesAgent
 import re
+from sqlalchemy.orm import Session
+from database import get_db  
+from models import Article  # models.py에서 Article 모델 가져오기
+from router import router
 
 app = FastAPI()
-
-embeddings = OllamaEmbeddings(model="mxbai-embed-large")
-vectorstore = Chroma(embedding_function=embeddings)
-
-class ArticleResponse(BaseModel):
-    short_summary: str
-    medium_summary: str
-    current_index: int
-
-current_index = 168559  
-
-@app.get("/latest_article")
-async def get_latest_article():
-    global current_index  
-    agent = AITimesAgent(start_idx=current_index)
-    article_data = agent.crawl_next_article()
-
-    if article_data:
-        content = article_data['content']
-        
-        content = content.replace('\n', ' ').replace('\r', ' ').replace('\\n', ' ').replace('\t', ' ').strip()
-        content = re.sub(r'\s+', ' ', content)
-
-        docs = [Document(page_content=content)]
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-        splits = text_splitter.split_documents(docs)
-        vectorstore.add_documents(splits)
-
-        retrieved_docs = vectorstore.similarity_search(content)
-        if not retrieved_docs:
-            return JSONResponse(content={"error": "관련 문서를 찾을 수 없습니다."})
-
-        formatted_context = " ".join(doc.page_content for doc in retrieved_docs)
-
-        short_summary = ollama.chat(model='Llama-Koren',
-                                     messages=[
-                                         {"role": "system", "content": "Summarize the following text briefly in Korean."},
-                                         {"role": "user", "content": formatted_context}])['message']['content']
-
-        medium_summary = ollama.chat(model='Llama-Koren',
-                                      messages=[
-                                          {"role": "system", "content": "Summarize the following text in Korean."},
-                                          {"role": "user", "content": formatted_context}])['message']['content']
-
-        short_summary = short_summary.replace('\n', ' ').replace('\\n', ' ').strip()
-        medium_summary = medium_summary.replace('\n', ' ').replace('\\n', ' ').strip()
-
-        current_index += 1
-
-        return ArticleResponse(short_summary=short_summary, medium_summary=medium_summary, current_index = current_index)
-    else:
-        return JSONResponse(content={"error": "최신 기사를 찾을 수 없습니다."})
-
+app.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
