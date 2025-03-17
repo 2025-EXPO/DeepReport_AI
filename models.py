86fc04ca4f69bfe2c7a27c0c9e46addee1c46fe8
Park jaemin <hungryshark4292@gmail.com>
2025-03-17 23:13:10 +0900

    models

---
+++ models.py
@@ -0,0 +1,12 @@
+# models.py
+
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Article(Base):
+    __tablename__ = "articles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    short_summary = Column(String)
+    medium_summary = Column(String)
+    current_index = Column(Integer)
