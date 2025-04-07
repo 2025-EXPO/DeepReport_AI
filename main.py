223ab429b5a0e591fba190dd090b22db7bc646e9
Park jaemin <hungryshark4292@gmail.com>
2025-04-08 08:50:53 +0900

    main

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-# main.py
 from fastapi import FastAPI
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 import router
-from latest_article import fetch_and_store_latest_article
-import router.router
-import router.sse
-import router.gemini
-import logging
 from router.sse import run_async_job
+import logging
 from fastapi.middleware.cors import CORSMiddleware
-
-
+from contextlib import asynccontextmanager
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -19,18 +13,34 @@ logger = logging.getLogger(__name__)
 executors = {
     'default': {'type': 'threadpool', 'max_workers': 20}
 }
-
 scheduler = AsyncIOScheduler(executors=executors)
 
-app = FastAPI()
-app.include_router(router.router.router)
-app.include_router(router.sse.router)
-# app.include_router(router.gemini.router)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    if not scheduler.running:
+        try:
+            scheduler.remove_job('news_crawler_job')
+        except Exception as e:
+            logger.warning(f"기존 작업 제거 중 오류 발생: {e}")
+        
+        scheduler.add_job(
+            run_async_job,
+            trigger=IntervalTrigger(minutes=1),
+            id='news_crawler_job',
+            max_instances=1
+        )
+        scheduler.start()
+        logger.info("스케줄러 시작: 1분마다 뉴스 크롤링 및 SSE 알림")
+    
+    yield  
 
-origins = [
-    "*"
-]
+    if scheduler.running:
+        scheduler.shutdown()
+        logger.info("스케줄러 종료")
 
+app = FastAPI(lifespan=lifespan)
+
+origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -39,36 +49,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(router.router.router)
+app.include_router(router.sse.router)
+# app.include_router(router.gemini.router)
 
-@app.get('/')
+@app.get("/")
 def get_main():
-    return {'message': 'welcome modeep'}
-
-@app.on_event("startup")
-async def start_scheduler():
-    if not scheduler.running:
-        try:
-            scheduler.remove_job('news_crawler_job')
-        except Exception as e:
-            logger.warning(f"기존 작업 제거 중 오류 발생: {e}")
-            
-        # 동기 래퍼 함수 스케줄링
-        scheduler.add_job(
-            run_async_job,
-            trigger=IntervalTrigger(minutes=30),
-            id='news_crawler_job',
-            max_instances=1
-        )
-            
-        scheduler.start()
-        logger.info("스케줄러 시작: 1분마다 뉴스 크롤링 및 SSE 알림")
-
-@app.on_event("shutdown")
-async def shutdown_scheduler():
-    if scheduler.running:
-        scheduler.shutdown()
-        logger.info("스케줄러 종료")
-
+    return {"message": "welcome modeep"}
 
 if __name__ == "__main__":
     import uvicorn
