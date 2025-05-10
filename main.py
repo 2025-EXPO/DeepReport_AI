c880c60d71a5eaf92f82ea04fe94656b5755c414
Park jaemin <hungryshark4292@gmail.com>
2025-05-10 23:56:43 +0900

    뉴스 크롤링 시간 조절

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ async def lifespan(app: FastAPI):
 
         scheduler.add_job(
             run_async_job,
-            trigger=IntervalTrigger(minutes=10),
+            trigger=IntervalTrigger(minutes=100),
             id="news_crawler_job",
             max_instances=1,
         )
